717b284527fde2c219ec6141b0c44e78f80088a9
cchoijjinyoung <cgy7127@naver.com>
2023-04-06 01:24:45 +0900

    Hello World

---
+++ src/main/java/com/logic/nemonemo/NemonemoApplication.java
@@ -0,0 +1,13 @@
+package com.logic.nemonemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemonemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemonemoApplication.class, args);
+	}
+
+}
