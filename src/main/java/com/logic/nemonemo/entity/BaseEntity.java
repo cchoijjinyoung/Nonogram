0992288602a5ffe05e6058fd54ac840d03e7a88f
cchoijjinyoung <cgy7127@naver.com>
2023-04-25 19:27:37 +0900

    modify : BaseEntity 수정

--- src/main/java/com/logic/nemonemo/entity/BaseEntity.java
+++ src/main/java/com/logic/nemonemo/entity/BaseEntity.java
@@ -1,8 +1,24 @@
 package com.logic.nemonemo.entity;
 
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-public class BaseEntity {
-    private LocalDateTime createDate;
-    private LocalDateTime updateDate;
+@MappedSuperclass
+@EntityListeners(value = { AuditingEntityListener.class })
+@Getter
+abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "regDate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "modDate")
+    private LocalDateTime modDate;
 }
